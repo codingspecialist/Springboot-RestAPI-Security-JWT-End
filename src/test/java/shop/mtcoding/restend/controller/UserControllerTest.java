9db699c5a2a2fbc061938bf2c632f485b28f967b
getinthere <getinthere@naver.com>
2023-04-24 18:40:03 +0900

    라스트

--- src/test/java/shop/mtcoding/restend/controller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/restend/controller/UserControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,7 +20,6 @@ import shop.mtcoding.restend.model.user.UserRepository;
 import javax.persistence.EntityManager;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
