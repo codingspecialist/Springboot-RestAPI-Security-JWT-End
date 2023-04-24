73974fd2a6b1a7edb52e3caf8851e5ff05c3f6a3
getinthere <getinthere@naver.com>
2023-04-24 09:49:14 +0900

    프로젝트 세팅 완료

---
+++ src/main/java/shop/mtcodng/restend/RestendApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcodng.restend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestendApplication.class, args);
+    }
+
+}
